d32b09a7b438a49608cff6f234d76e1df63f14b7
fmachadopiriz <fmachadopiriz@outlook.com>
2018-07-19 09:46:14 -0300

    Initial commit

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+using System.Reactive;
+using System.Diagnostics;
+
+namespace AvaloniaTest.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        // public string Greeting => "Hello World!";
+        // public string Greeting { get; } = "Hello World!";
+        public string Greeting
+        {
+            get
+            {
+                return "Hello World!";
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            DoTheThing = ReactiveCommand.Create(RunTheThing);
+        }
+
+        public ReactiveCommand<Unit, Unit> DoTheThing { get; }
+
+        void RunTheThing()
+        {
+            Debug.Print("Hello from run the thing!");
+        }
+    }
+}
